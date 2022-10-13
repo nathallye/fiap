635bfedf074e07644f76292e60b3d8f42506ae78
nathallyet <nathallyet@gmail.com>
2022-10-13 20:45:22 -0300

    README.md fiap on

--- fiap-on/README.md
+++ fiap-on/README.md
@@ -5,7 +5,10 @@ Studies on Programming Logic with TypeScript, HTML5, CC3, JavaScript, Node.js an
   - ### [Introdução ao Desenvolvimento Web](web-development/README.md)
   - ### [HTML5](html5-start/README.md)
   - ### [CSS3](css3/README.md)
-
+  - ### [Bootstrap](bootstrap/README.md)
+  - ### [JavaScript](javascript/README.md)
+  - ### [jQuery](jquery/README.md)
   - ### [Git & GitHub](git-github/README.md)
+  - ### [React.JS](react/README.md)
   - ### [Metodologias Ágeis](agil-methodologies/README.md)
 
--- fiap-with-teacher/03-react/day04/next/pages/login.tsx
+++ fiap-with-teacher/03-react/day04/next/pages/login.tsx
@@ -1,9 +1,56 @@
 import React from "react";
+import Link from "next/link";
+
+import { Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+type CopyProps = {
+  site: string;
+  sx?: object;
+}
+
+function Copyright(props: CopyProps) {
+  console.log("props", props);
+  console.log(props.site);
+
+  return (
+    <Typography>
+      {"Copyright ©️ "}
+      {/*inherit - hende a cor do elemento pai*/}
+      <Link color="inherit" href="https://www.avanade.com.br/">
+        {props.site}
+      </Link>{" "}
+      {new Date().getFullYear()} {/*Função para pegar o ano atual*/}
+      {"."}
+    </Typography>
+  )
+}
+
+const theme = createTheme();
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1>LoginPage</h1>
-    </div>
+    <ThemeProvider theme={theme}> {/*ThemeProvider - faz o dark automáticamente*/}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline>
+          <Box sx={{mt: 8, display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <Typography component="h1" variant="h5"> {/*Para texto*/}
+              Login
+            </Typography>
+            <Box component="form" onSubmit={() => {console.log("enviado")}}>
+              <TextField margin="normal" required fullWidth id="email" label="Digite o e-mail" name="email"
+                autoComplete="email" autoFocus />
+              <TextField margin="normal" required fullWidth id="password" label="Digite a senha" name="password"
+                autoComplete="current-password" autoFocus />
+            </Box>
+          </Box>
+        </CssBaseline>
+        <Copyright site="www.avanade.com.br" sx={{mt:8, mb: 4}} />
+      </Container>
+    </ThemeProvider>
   )
 }
